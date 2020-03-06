c73a49031836c27b6c6d4024040615ff280bc4cb
jaihon1 <jaihon.qasim@hotmail.com>
2020-03-06 17:12:59 -0500

    Setup Done.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
-def main():
-    print("This is the main function")
+from modules.naive-bayes-v1 import NaiveBayes
 
+def main():
+
+    ## Reading test file
+    test_file = '../datasets/testing/test-tweets-given.txt'
+
+    with open(test_file) as f:
+        tweets = f.readlines()
+
+    for tweet in tweets:
+        print(tweet)
 
 if __name__ == '__main__':
     main()
