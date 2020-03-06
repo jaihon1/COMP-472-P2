eafe3107b61ed4ba75f558073724e0cab86b71b0
jaihon1 <jaihon.qasim@hotmail.com>
2020-03-06 17:33:15 -0500

    Started creation of model class.

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
-from modules.naive-bayes-v1 import NaiveBayes
+from modules.naiveBayes import NaiveBayes
 
 def main():
 
-    ## Reading test file
-    test_file = '../datasets/testing/test-tweets-given.txt'
+    ## Reading files
+    test_file = '../../datasets/testing/test-tweets-given.txt'
+    train_file = '../../datasets/testing/training-tweets.txt'
 
-    with open(test_file) as f:
-        tweets = f.readlines()
+    model = NaiveBayes(2, 1, 1, test_file, train_file)
 
-    for tweet in tweets:
-        print(tweet)
+    print(model.getCorpus())
+
+
+
+    # with open(test_file) as f:
+    #     tweets = f.readlines()
+
+    # for tweet in tweets:
+    #     print(tweet)
 
 if __name__ == '__main__':
     main()
--- src/modules/naiveBayes.py
+++ src/modules/naiveBayes.py
@@ -11,17 +11,23 @@
 ### Smoothing
 # s = {0,...,1}
 
+import string
+import numpy as np
 
 class NaiveBayes():
     def __init__(self, v, n, s, training_file_name, testing_file_name):
-        self.vocabulary = v
-        self.n_gram = n
+        self.vocabulary_type = v
+        self.corpus = None
+        self.n_gram_type = n
+        self.n_gram = None
         self.smoothing = s
         self.training_file_name = training_file_name
         self.testing_file_name = testing_file_name
 
-    def getVocabulary(self):
-        return self.vocabulary
+        self.generateVocabulary()
+
+    def getCorpus(self):
+        return self.corpus
 
     def getNGram(self):
         return self.n_gram
@@ -31,6 +37,18 @@ class NaiveBayes():
 
     def getTestingFile(self):
         return self.testing_file_name
+
+
+    def generateVocabulary(self):
+        if self.vocabulary_type == 0:
+            self.corpus = list(string.ascii_lowercase)
+
+        elif self.vocabulary_type == 1:
+            self.corpus = list(string.ascii_uppercase)
+
+        elif self.vocabulary_type == 2:
+            self.corpus = list(string.ascii_letters)
+
 
 def main():
     print("Naive main.")
