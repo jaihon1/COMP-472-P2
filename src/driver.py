3830d1f836439dbdc6d62585579a9b81762561f2
jaihon1 <jaihon.qasim@hotmail.com>
2020-03-30 23:28:51 -0400

    Cleaned up BOYD. Completed end-to-end for BOYD.

--- src/driver.py
+++ src/driver.py
@@ -5,19 +5,19 @@ def main():
     ## Reading files
     test_file = '/Users/dzhay/Github/COMP-472-P2/datasets/test/test-tweets-given.txt'
     train_file = '/Users/dzhay/Github/COMP-472-P2/datasets/train/training-tweets.txt'
-    train_dataset = '/Users/dzhay/Github/COMP-472-P2/datasets/train/train-encoded-spaced-full.txt'
-    test_dataset = '/Users/dzhay/Github/COMP-472-P2/datasets/test/test-encoded-spaced-full.txt'
+    # train_dataset = '/Users/dzhay/Github/COMP-472-P2/datasets/train/train-encoded-spaced-full.txt'
+    # test_dataset = '/Users/dzhay/Github/COMP-472-P2/datasets/test/test-encoded-spaced-full.txt'
 
-    train_output = '/Users/dzhay/Github/COMP-472-P2/datasets/train/train-output-full.txt'
-    test_ouput = '/Users/dzhay/Github/COMP-472-P2/datasets/test/test-output-full.txt'
+    # train_output = '/Users/dzhay/Github/COMP-472-P2/datasets/train/train-output-full.txt'
+    # test_ouput = '/Users/dzhay/Github/COMP-472-P2/datasets/test/test-output-full.txt'
     vocabulary = 0
 
-    model = NeuralNet(vocabulary, train_file, test_file, train_dataset, test_dataset, train_output, test_ouput)
+
+    model = NeuralNet(vocabulary, train_file, test_file)
 
 
-    # model.cleanData()
-    # model.runTest()
-    # model.textToCsv()
+    model.cleanTrainData()
+    model.cleanTestData()
 
     model.train()
     model.runTest()
