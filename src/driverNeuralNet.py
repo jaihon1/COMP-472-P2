dbb77aa291fb316d46539e1063ed248bd59c3c25
jaihon1 <jaihon.qasim@hotmail.com>
2020-04-04 21:31:58 -0400

    Extra finalizing...

--- src/driverNeuralNet.py
+++ src/driverNeuralNet.py
@@ -7,22 +7,23 @@ def main():
     train_file = '/Users/dzhay/Github/COMP-472-P2/datasets/train/training-tweets.txt'
     vocabulary = 0
 
+    # Setup
     model = NeuralNet(vocabulary, train_file, test_file)
-
     start_time = time.time()
 
+    # Start Model
     model.cleanTrainData()
     model.cleanTestData()
-    # clean_time = time.time() - start_time
-    # print("--- Duration of Cleaning: %s seconds ---" % (clean_time))
+    clean_time = time.time() - start_time
+    print("--- Duration of Cleaning: %s seconds ---" % (clean_time))
 
     model.train()
-    # train_time = time.time() - start_time - clean_time
-    # print("--- Duration of Training: %s seconds ---" % (train_time))
+    train_time = time.time() - start_time - clean_time
+    print("--- Duration of Training: %s seconds ---" % (train_time))
 
     model.runTest()
-    # test_time = time.time() - start_time - train_time
-    # print("--- Duration of Testing: %s seconds ---" % (test_time))
+    test_time = time.time() - start_time - train_time
+    print("--- Duration of Testing: %s seconds ---" % (test_time))
 
 if __name__ == '__main__':
     main()
